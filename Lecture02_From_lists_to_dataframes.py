209c372b7790a72922a2affe7cfdd3bf53537ff2
Ramez <MHDRAMEZ.ALRAWAS@BTC-AG.COM>
2018-10-16 16:46:33 +0200

    lecture02 outlines

--- Lecture02_From_lists_to_dataframes.py
+++ Lecture02_From_lists_to_dataframes.py
@@ -1,1 +1,7 @@
 #Lecture02
+""" In the curent notebook we are going to explain:
+What is Pandas and why do we want to start using it in data analytics?
+Data structures in Python and in Pandas
+Performance in Pandas (sort, max, mean)
+When to use Pandas
+Data Types """
