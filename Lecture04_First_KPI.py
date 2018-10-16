cda0d033a71842156102da332e696b584a86cdcf
Ramez <MHDRAMEZ.ALRAWAS@BTC-AG.COM>
2018-10-16 16:58:47 +0200

    Content of theis lecture

---
+++ Lecture04_First_KPI.py
@@ -0,0 +1,9 @@
+#Lecture04
+""" Content of the current notebook:
+How to load data from CSV or Excel files into Pandas Data Frames
+Data types in Pandas
+Deal with gaps in the data
+Find solution to the CEO question:
+"What are the most sold products?"
+Visualize data
+New CEO request """
